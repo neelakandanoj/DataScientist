fc45f799ec89984d2edaaeb2b4ec6b36d8b468c8
neelakandan <neelakandan2001kpm@gmail.com>
2024-04-27 08:17:37 +0530

    Day2

--- Python/Day2/main.py
+++ Python/Day2/main.py
@@ -1,23 +1,23 @@
-# i=0
-# while i<3:
+i=0
+while i<3:
 
-#     todo=input("Enter the details: ")
-#     print(todo)
-#     i+=1
+    todo=input("Enter the details: ")
+    print(todo)
+    i+=1
 
 #To do
-# todos=[]
-# while True:
-#     todo=input("Enter the items to work: ")
-#     todos.append(todo)
-#     print(todos)
+todos=[]
+while True:
+    todo=input("Enter the items to work: ")
+    todos.append(todo)
+    print(todos)
 
 
 #Password generator
-# password=input("enter the password: ")
-# while password!="pass_2024":
-#     print("this is new password")
-# print("this is previous password")
+password=input("enter the password: ")
+while password!="pass_2024":
+    print("this is new password")
+print("this is previous password")
 
 
 
